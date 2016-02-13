52bfe24eda5e6e6150fac5e7244812282aac5c00
AbhimanyuSjaitawat <abhi1994jaitawat@gmail.com>
2016-02-13 16:44:00 +0530

    added some comments

--- break_time.py
+++ break_time.py
@@ -5,7 +5,7 @@ i = 0;
 while i < 3:
             print("program starting at "+time.ctime())
            
-
+            #break of 50 seconds
             time.sleep(50)
             webbrowser.open("https://www.youtube.com/watch?v=SmM0653YvXU")
             i = i+1
