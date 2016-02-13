39dfa33a88b9e9460c942f9584ca388a85585980
AbhimanyuSjaitawat <abhi1994jaitawat@gmail.com>
2016-02-13 16:39:21 +0530

    break between

---
+++ break_time.py
@@ -0,0 +1,12 @@
+
+import time
+import webbrowser
+i = 0;
+while i < 3:
+            print("program starting at "+time.ctime())
+           
+
+            time.sleep(50)
+            webbrowser.open("https://www.youtube.com/watch?v=SmM0653YvXU")
+            i = i+1
+
