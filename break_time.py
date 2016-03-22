82dab076e51f97b8183e2c9dad9db4021739d425
Ishan Saxena <isaxena171@gmail.com>
2016-03-22 08:10:29 +0530

    Refactored break_time.py

--- break_time.py
+++ break_time.py
@@ -1,12 +1,9 @@
 
-import time
-import webbrowser
-i = 0;
-while i < 3:
+import time, webbrowser
+
+for i in range(3):
             print("program starting at "+time.ctime())
-           
             #break of 50 seconds
             time.sleep(50)
             webbrowser.open("https://www.youtube.com/watch?v=SmM0653YvXU")
-            i = i+1
 
